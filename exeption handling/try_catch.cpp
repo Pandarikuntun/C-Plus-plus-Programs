0828483a457b734ca3e31db720b95fb9fc30037e
Pandari kuntun <pandarikuntun@gmail.com>
2025-11-06 13:55:14 +0530

    Add files via upload

---
+++ exeption handling/try_catch.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int age;
+    cout<<"enter age";
+    cin>>age;
+    try {
+  
+  if (age >= 18) {
+    cout << "Access granted - you are old enough.";
+  } else {
+    throw age;
+  }
+}
+catch (int myNum) {
+  cout << "Access denied - You must be at least 18 years old.\n";
+  cout << "Age is: " << myNum;
+}
+
+return 0;
+}
+
